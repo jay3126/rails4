36c012ff74df07f63f9d43d98ac8e56a4b7ecc12
Vinay <vinay59.grewal@gmail.com>
2013-03-03 22:40:08 +0530

    create post model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mogoid::Application.config.secret_token = '9f7b5a44df43ec1bd01ce2faf293a4fe73b80236e1c5962279e901b8735372633190a1bd45ae3eb3c6c845950f26cd9d76c837b2ef73969e13c6c26faa030782'
